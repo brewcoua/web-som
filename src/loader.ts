fd5349e101c31952af8eda215b7ebbaf8dd8cad8
Brewen Couaran <45310490+brewcoua@users.noreply.github.com>
2024-07-01 13:15:33 +0200

    fix: use custom tags & remove duplicate rects

--- src/loader.ts
+++ src/loader.ts
@@ -1,95 +1,121 @@
-import { SELECTORS } from "./constants";
-import InteractiveElements from "./domain/InteractiveElements";
-import { VisibilityFilter, NestingFilter } from "./filters";
+import { EDITABLE_SELECTORS, SELECTORS } from './constants';
+import InteractiveElements from './domain/InteractiveElements';
+import { VisibilityFilter, NestingFilter } from './filters';
 
 export default class Loader {
-  private readonly filters = {
-    visibility: new VisibilityFilter(),
-    nesting: new NestingFilter(),
-  };
+	private readonly filters = {
+		visibility: new VisibilityFilter(),
+		nesting: new NestingFilter(),
+	};
 
-  async loadElements() {
-    const selector = SELECTORS.join(",");
+	async loadElements() {
+		const selector = SELECTORS.join(',');
 
-    let fixedElements = Array.from(
-      document.querySelectorAll(selector)
-    ) as HTMLElement[];
+		let fixedElements = Array.from(
+			document.querySelectorAll(selector)
+		) as HTMLElement[];
 
-    // Let's also do a querySelectorAll inside all the shadow roots (for custom elements, e.g. reddit)
-    const shadowRoots = this.shadowRoots();
-    for (let i = 0; i < shadowRoots.length; i++) {
-      fixedElements = fixedElements.concat(
-        Array.from(shadowRoots[i].querySelectorAll(selector))
-      );
-    }
+		// Let's also do a querySelectorAll inside all the shadow roots (for custom elements, e.g. reddit)
+		const shadowRoots = this.shadowRoots();
+		for (let i = 0; i < shadowRoots.length; i++) {
+			fixedElements = fixedElements.concat(
+				Array.from(shadowRoots[i].querySelectorAll(selector))
+			);
+		}
 
-    let unknownElements: HTMLElement[] = [];
-    const walker = document.createTreeWalker(
-      document.body,
-      NodeFilter.SHOW_ELEMENT,
-      {
-        acceptNode() {
-          return NodeFilter.FILTER_ACCEPT;
-        },
-      }
-    );
+		let unknownElements: HTMLElement[] = [];
+		const walker = document.createTreeWalker(
+			document.body,
+			NodeFilter.SHOW_ELEMENT,
+			{
+				acceptNode() {
+					return NodeFilter.FILTER_ACCEPT;
+				},
+			}
+		);
 
-    let node: Node | null;
-    while ((node = walker.nextNode())) {
-      const el = node as HTMLElement;
-      if (
-        !el.matches(selector) &&
-        window.getComputedStyle(el).cursor === "pointer"
-      ) {
-        unknownElements.push(el);
-      }
-    }
+		let node: Node | null;
+		while ((node = walker.nextNode())) {
+			const el = node as HTMLElement;
+			if (
+				!el.matches(selector) &&
+				window.getComputedStyle(el).cursor === 'pointer'
+			) {
+				unknownElements.push(el);
+			}
+		}
 
-    unknownElements = Array.from(unknownElements)
-      .filter((element, index, self) => self.indexOf(element) === index)
-      .filter(
-        (element) =>
-          !element.closest("svg") &&
-          !fixedElements.some((el) => el.contains(element))
-      );
+		unknownElements = Array.from(unknownElements)
+			.filter((element, index, self) => self.indexOf(element) === index)
+			.filter(
+				(element) =>
+					!element.closest('svg') &&
+					!fixedElements.some((el) => el.contains(element))
+			);
 
-    let interactive: InteractiveElements = {
-      fixed: fixedElements,
-      unknown: unknownElements,
-    };
+		let interactive: InteractiveElements = {
+			fixed: fixedElements,
+			unknown: unknownElements,
+		};
 
-    console.groupCollapsed("Elements");
-    console.log("Before filters", interactive);
+		console.groupCollapsed('Elements');
+		console.log('Before filters', interactive);
 
-    interactive = await this.filters.visibility.apply(interactive);
-    console.log("After visibility filter", interactive);
+		interactive = await this.filters.visibility.apply(interactive);
+		console.log('After visibility filter', interactive);
 
-    interactive = await this.filters.nesting.apply(interactive);
-    console.log("After nesting filter", interactive);
-    console.groupEnd();
+		interactive = await this.filters.nesting.apply(interactive);
+		console.log('After nesting filter', interactive);
+		console.groupEnd();
 
-    return interactive.fixed.concat(interactive.unknown);
-  }
+		return interactive.fixed
+			.concat(interactive.unknown)
+			.reduce((acc: HTMLElement[], el: HTMLElement) => {
+				// Remove all elements that have the same rect, while keeping either the editable one, then the first one
+				const rect = el.getBoundingClientRect();
+				const sameRect = acc.filter(
+					(element) =>
+						element.getBoundingClientRect().top === rect.top &&
+						element.getBoundingClientRect().left === rect.left
+				);
 
-  shadowRoots() {
-    const shadowRoots: ShadowRoot[] = [];
-    const walker = document.createTreeWalker(
-      document.body,
-      NodeFilter.SHOW_ELEMENT,
-      {
-        acceptNode(node) {
-          return NodeFilter.FILTER_ACCEPT;
-        },
-      }
-    );
+				if (sameRect.length > 0) {
+					const editable = sameRect.find(
+						(element) =>
+							element.isContentEditable ||
+							EDITABLE_SELECTORS.some((selector) => element.matches(selector))
+					);
 
-    let node: Node | null;
-    while ((node = walker.nextNode())) {
-      if (node && (node as Element).shadowRoot) {
-        shadowRoots.push((node as Element).shadowRoot as ShadowRoot);
-      }
-    }
+					if (editable) {
+						return acc.filter((element) => element !== editable).concat(el);
+					}
 
-    return shadowRoots;
-  }
+					return acc;
+				}
+
+				return acc.concat(el);
+			}, [] as HTMLElement[]);
+	}
+
+	shadowRoots() {
+		const shadowRoots: ShadowRoot[] = [];
+		const walker = document.createTreeWalker(
+			document.body,
+			NodeFilter.SHOW_ELEMENT,
+			{
+				acceptNode(node) {
+					return NodeFilter.FILTER_ACCEPT;
+				},
+			}
+		);
+
+		let node: Node | null;
+		while ((node = walker.nextNode())) {
+			if (node && (node as Element).shadowRoot) {
+				shadowRoots.push((node as Element).shadowRoot as ShadowRoot);
+			}
+		}
+
+		return shadowRoots;
+	}
 }
--- src/ui.ts
+++ src/ui.ts
@@ -1,205 +1,206 @@
-import { EDITABLE_SELECTORS, SURROUNDING_RADIUS } from "./constants";
-import UIColors, { Color } from "./ui/colors";
+import { EDITABLE_SELECTORS, SURROUNDING_RADIUS } from './constants';
+import UIColors, { Color } from './ui/colors';
 
 type SimpleDOMRect = {
-  top: number;
-  bottom: number;
-  left: number;
-  right: number;
-  width: number;
-  height: number;
+	top: number;
+	bottom: number;
+	left: number;
+	right: number;
+	width: number;
+	height: number;
 };
 
 export default class UI {
-  private readonly colors = new UIColors();
+	private readonly colors = new UIColors();
 
-  display(elements: HTMLElement[]) {
-    const labels: SimpleDOMRect[] = [];
-    const boundingBoxes: (SimpleDOMRect & {
-      color: Color;
-    })[] = [];
-    const rawBoxes: HTMLElement[] = [];
+	display(elements: HTMLElement[]) {
+		const labels: SimpleDOMRect[] = [];
+		const boundingBoxes: (SimpleDOMRect & {
+			color: Color;
+		})[] = [];
+		const rawBoxes: HTMLElement[] = [];
 
-    // First, define the bounding boxes
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
+		// First, define the bounding boxes
+		for (let i = 0; i < elements.length; i++) {
+			const element = elements[i];
 
-      const rect = element.getBoundingClientRect();
-      const div = document.createElement("div");
-      div.style.left = `${rect.left}px`;
-      div.style.top = `${rect.top}px`;
-      div.style.width = `${rect.width}px`;
-      div.style.height = `${rect.height}px`;
-      div.classList.add("SoM");
+			const rect = element.getBoundingClientRect();
+			const box = document.createElement('som-box');
+			box.style.left = `${rect.left}px`;
+			box.style.top = `${rect.top}px`;
+			box.style.width = `${rect.width}px`;
+			box.style.height = `${rect.height}px`;
+			box.classList.add('SoM');
 
-      // If the element is editable, add additional class
-      if (
-        element.isContentEditable ||
-        EDITABLE_SELECTORS.some((selector) =>
-          element.matches(selector as string)
-        )
-      ) {
-        div.classList.add("editable");
-      }
+			// If the element is editable, add additional class
+			if (
+				element.isContentEditable ||
+				EDITABLE_SELECTORS.some((selector) =>
+					element.matches(selector as string)
+				)
+			) {
+				box.classList.add('editable');
+			}
 
-      // To generate a color, we'll need to first get the colors of the surrounding boxes
-      const surroundingColors = boundingBoxes
-        .filter((box) => {
-          // Check if it is within SURROUNDING_RADIUS, from any of its corners
-          const distances = [
-            Math.sqrt(
-              Math.pow(rect.left - box.left, 2) +
-                Math.pow(rect.top - box.top, 2)
-            ),
-            Math.sqrt(
-              Math.pow(rect.right - box.right, 2) +
-                Math.pow(rect.top - box.top, 2)
-            ),
-            Math.sqrt(
-              Math.pow(rect.left - box.left, 2) +
-                Math.pow(rect.bottom - box.bottom, 2)
-            ),
-            Math.sqrt(
-              Math.pow(rect.right - box.right, 2) +
-                Math.pow(rect.bottom - box.bottom, 2)
-            ),
-          ];
+			// To generate a color, we'll need to first get the colors of the surrounding boxes
+			const surroundingColors = boundingBoxes
+				.filter((box) => {
+					// Check if it is within SURROUNDING_RADIUS, from any of its corners
+					const distances = [
+						Math.sqrt(
+							Math.pow(rect.left - box.left, 2) +
+								Math.pow(rect.top - box.top, 2)
+						),
+						Math.sqrt(
+							Math.pow(rect.right - box.right, 2) +
+								Math.pow(rect.top - box.top, 2)
+						),
+						Math.sqrt(
+							Math.pow(rect.left - box.left, 2) +
+								Math.pow(rect.bottom - box.bottom, 2)
+						),
+						Math.sqrt(
+							Math.pow(rect.right - box.right, 2) +
+								Math.pow(rect.bottom - box.bottom, 2)
+						),
+					];
 
-          return distances.some((distance) => distance < SURROUNDING_RADIUS);
-        })
-        .map((box) => box.color);
+					return distances.some((distance) => distance < SURROUNDING_RADIUS);
+				})
+				.map((box) => box.color);
 
-      const color = this.colors.contrastColor(element, surroundingColors);
+			const color = this.colors.contrastColor(element, surroundingColors);
 
-      // Set color as variable to be used in CSS
-      div.style.setProperty(
-        "--SoM-color",
-        `${color.r}, ${color.g}, ${color.b}`
-      );
+			// Set color as variable to be used in CSS
+			box.style.setProperty(
+				'--SoM-color',
+				`${color.r}, ${color.g}, ${color.b}`
+			);
 
-      document.body.appendChild(div);
+			document.body.appendChild(box);
 
-      boundingBoxes.push({
-        top: rect.top,
-        bottom: rect.bottom,
-        left: rect.left,
-        right: rect.right,
-        width: rect.width,
-        height: rect.height,
-        color: color,
-      });
+			boundingBoxes.push({
+				top: rect.top,
+				bottom: rect.bottom,
+				left: rect.left,
+				right: rect.right,
+				width: rect.width,
+				height: rect.height,
+				color: color,
+			});
 
-      rawBoxes.push(div);
-    }
+			rawBoxes.push(box);
+		}
 
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
-      const box = boundingBoxes[i];
+		for (let i = 0; i < elements.length; i++) {
+			const element = elements[i];
+			const box = boundingBoxes[i];
 
-      const label = document.createElement("label");
-      label.textContent = `${i}`;
-      label.style.color = this.getColorByLuminance(box.color);
+			const label = document.createElement('som-label');
+			label.textContent = `${i}`;
+			label.style.color = this.getColorByLuminance(box.color);
+			label.classList.add('SoM--label');
 
-      rawBoxes[i].appendChild(label);
+			rawBoxes[i].appendChild(label);
 
-      const labelRect = label.getBoundingClientRect();
+			const labelRect = label.getBoundingClientRect();
 
-      const gridSize = 10;
-      const positions: {
-        top: number;
-        left: number;
-      }[] = [];
-      for (let i = 0; i <= gridSize; i++) {
-        // Top side
-        positions.push({
-          top: box.top - labelRect.height,
-          left: box.left + (box.width / gridSize) * i - labelRect.width / 2,
-        });
-        // Bottom side
-        positions.push({
-          top: box.bottom,
-          left: box.left + (box.width / gridSize) * i - labelRect.width / 2,
-        });
-        // Left side
-        positions.push({
-          top: box.top + (box.height / gridSize) * i - labelRect.height / 2,
-          left: box.left - labelRect.width,
-        });
-        // Right side
-        positions.push({
-          top: box.top + (box.height / gridSize) * i - labelRect.height / 2,
-          left: box.right,
-        });
-      }
+			const gridSize = 10;
+			const positions: {
+				top: number;
+				left: number;
+			}[] = [];
+			for (let i = 0; i <= gridSize; i++) {
+				// Top side
+				positions.push({
+					top: box.top - labelRect.height,
+					left: box.left + (box.width / gridSize) * i - labelRect.width / 2,
+				});
+				// Bottom side
+				positions.push({
+					top: box.bottom,
+					left: box.left + (box.width / gridSize) * i - labelRect.width / 2,
+				});
+				// Left side
+				positions.push({
+					top: box.top + (box.height / gridSize) * i - labelRect.height / 2,
+					left: box.left - labelRect.width,
+				});
+				// Right side
+				positions.push({
+					top: box.top + (box.height / gridSize) * i - labelRect.height / 2,
+					left: box.right,
+				});
+			}
 
-      // Calculate score for each position
-      const scores = positions.map((position) => {
-        let score = 0;
+			// Calculate score for each position
+			const scores = positions.map((position) => {
+				let score = 0;
 
-        // Check if position is within bounds
-        if (
-          position.top < 0 ||
-          position.top + labelRect.height > window.innerHeight ||
-          position.left < 0 ||
-          position.left + labelRect.width > window.innerWidth
-        ) {
-          score += Infinity; // Out of bounds, set score to infinity
-        } else {
-          // Calculate overlap with other labels and bounding boxes
-          labels.concat(boundingBoxes).forEach((existing) => {
-            // Ignore bounding boxes that are fully covering the current box
-            if (
-              existing.top <= box.top &&
-              existing.bottom >= box.bottom &&
-              existing.left <= box.left &&
-              existing.right >= box.right
-            ) {
-              return;
-            }
+				// Check if position is within bounds
+				if (
+					position.top < 0 ||
+					position.top + labelRect.height > window.innerHeight ||
+					position.left < 0 ||
+					position.left + labelRect.width > window.innerWidth
+				) {
+					score += Infinity; // Out of bounds, set score to infinity
+				} else {
+					// Calculate overlap with other labels and bounding boxes
+					labels.concat(boundingBoxes).forEach((existing) => {
+						// Ignore bounding boxes that are fully covering the current box
+						if (
+							existing.top <= box.top &&
+							existing.bottom >= box.bottom &&
+							existing.left <= box.left &&
+							existing.right >= box.right
+						) {
+							return;
+						}
 
-            const overlapWidth = Math.max(
-              0,
-              Math.min(
-                position.left + labelRect.width,
-                existing.left + existing.width
-              ) - Math.max(position.left, existing.left)
-            );
-            const overlapHeight = Math.max(
-              0,
-              Math.min(
-                position.top + labelRect.height,
-                existing.top + existing.height
-              ) - Math.max(position.top, existing.top)
-            );
-            score += overlapWidth * overlapHeight; // Add overlap area to score
-          });
-        }
+						const overlapWidth = Math.max(
+							0,
+							Math.min(
+								position.left + labelRect.width,
+								existing.left + existing.width
+							) - Math.max(position.left, existing.left)
+						);
+						const overlapHeight = Math.max(
+							0,
+							Math.min(
+								position.top + labelRect.height,
+								existing.top + existing.height
+							) - Math.max(position.top, existing.top)
+						);
+						score += overlapWidth * overlapHeight; // Add overlap area to score
+					});
+				}
 
-        return score;
-      });
+				return score;
+			});
 
-      // Select position with lowest score
-      const bestPosition = positions[scores.indexOf(Math.min(...scores))];
+			// Select position with lowest score
+			const bestPosition = positions[scores.indexOf(Math.min(...scores))];
 
-      // Set label position
-      label.style.top = `${bestPosition.top - box.top}px`;
-      label.style.left = `${bestPosition.left - box.left}px`;
+			// Set label position
+			label.style.top = `${bestPosition.top - box.top}px`;
+			label.style.left = `${bestPosition.left - box.left}px`;
 
-      // Add the new label's position to the array
-      labels.push({
-        top: bestPosition.top,
-        left: bestPosition.left,
-        right: bestPosition.left + labelRect.width,
-        bottom: bestPosition.top + labelRect.height,
-        width: labelRect.width,
-        height: labelRect.height,
-      });
+			// Add the new label's position to the array
+			labels.push({
+				top: bestPosition.top,
+				left: bestPosition.left,
+				right: bestPosition.left + labelRect.width,
+				bottom: bestPosition.top + labelRect.height,
+				width: labelRect.width,
+				height: labelRect.height,
+			});
 
-      element.setAttribute("data-SoM", `${i}`);
-    }
-  }
+			element.setAttribute('data-SoM', `${i}`);
+		}
+	}
 
-  getColorByLuminance(color: Color) {
-    return color.luminance() > 0.5 ? "black" : "white";
-  }
+	getColorByLuminance(color: Color) {
+		return color.luminance() > 0.5 ? 'black' : 'white';
+	}
 }
